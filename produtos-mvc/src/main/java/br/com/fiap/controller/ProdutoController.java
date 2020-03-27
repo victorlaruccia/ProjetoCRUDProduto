707355bf7b64f193ea60c3c7f8750a3a3eb94d0f
victorlaruccia <victorlaruccia10@outlook.com>
2020-03-27 09:25:45 -0300

    Adicionado o cadastro de produto

---
+++ produtos-mvc/src/main/java/br/com/fiap/controller/ProdutoController.java
@@ -0,0 +1,45 @@
+package br.com.fiap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.fiap.model.ProdutoModel;
+import br.com.fiap.repository.ProdutoRepository;
+
+@Controller
+public class ProdutoController {
+	
+	ProdutoRepository repository = new ProdutoRepository();
+
+	// Busca
+	@RequestMapping(value = "/produto", method = RequestMethod.GET)
+	public String findAll(Model model) {
+
+		model.addAttribute("produtos", repository.findAll());
+		return "produtos";
+	}
+ 
+	@RequestMapping(value = "/produto/{id}", method = RequestMethod.GET)
+	public String findById(@PathVariable("id") long id, Model model) {
+		
+		model.addAttribute("produto", repository.findById(id));
+		return "produto-detalhe";//tem q ser o msm nome do jsp igualzinho, senão não funciona
+	}
+
+	@RequestMapping(value = "/produto/new", method = RequestMethod.GET)
+	public String openSave() {
+		return "produto-novo";
+	}
+	
+	@RequestMapping(value = "/produto/new", method = RequestMethod.POST)
+	public String save(ProdutoModel produtoModel) {
+		
+		repository.save(produtoModel);
+		return "produto-novo-sucesso";
+	}
+	
+	
+}
